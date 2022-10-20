d5fd2844d3e39dbf11bbb8d333f3af132e7893eb
Sabfes <bitaev.db@gmail.com>
2022-10-20 23:59:09 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,13 @@ import styles from './App.module.css';
 import {useEffect, useState} from "react";
 import Header from "./conponents/Header/Header";
 import axios from "axios";
+import Sidebar from "./conponents/Sidebar/Sidebar";
+import Task from "./conponents/Task/Task";
 
 function App() {
   const [posts, setPosts] = useState([])
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
 
   useEffect(() => {
     axios.get(`http://localhost:8000/api/posts`)
@@ -16,32 +20,54 @@ function App() {
     .catch(e => {
       console.log(e)
     })
-  })
+  }, [])
+
+  const addNewPost = () => {
+    axios.post('http://localhost:8000/api/posts', {
+      content,
+      title,
+      author: 'Denis'
+    }).then(res => {
+      console.log(res)
+      if (res.status === 200) {
+        setPosts([...posts, res.data])
+        setTitle('')
+        setContent('')
+      }
+    }).catch(e => {
+      console.log(e)
+    })
+  }
 
   return (
     <div className={styles.App}>
-      <Header/>
+      <Sidebar/>
 
-      <div className={styles.Cont}>
-        {
-          posts.length > 0 && <div>
-            {
-              posts.map(el => {
-                return <div className={styles.Post}>
-                  <span>
-                   Author: {el.author}
-                  </span>
-                  <span>
-                   Title: {el.title}
-                  </span>
-                  <span>
-                    Content: {el.content}
-                  </span>
-                </div>
-              })
-            }
-          </div>
-        }
+      <div className={styles.mainCont}>
+        <Header/>
+
+        <div className={styles.Cont}>
+          <p className={styles.Cont__title}>
+            v Today {posts.length && `(${posts.length})`}
+          </p>
+
+          {
+            posts.length > 0 && <div>
+              {
+                posts.map(el => {
+                  return <Task
+                    el={el}
+                    key={el._id}
+                  />
+                })
+              }
+            </div>
+          }
+        </div>
+      </div>
+
+      <div className={styles.contInfo}>
+
       </div>
     </div>
   );
--- client/src/conponents/Header/Header.jsx
+++ client/src/conponents/Header/Header.jsx
@@ -3,7 +3,11 @@ import styles from './Header.module.css'
 
 const Header = () => {
   return <div className={styles.Header}>
-    <span className={styles.Header__logo}>TL</span>
+    <h1 className={styles.Header__title}>All Tasks</h1>
+
+    <div className={styles.addBtn}><span>
+      +
+    </span></div>
   </div>
 }
 export default Header
